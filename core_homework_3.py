61e7354d33c45d2de2dd0adf57a85207f3f71598
Stanislav Rakytianskyi <mail.sovik@gmail.com>
2024-10-05 21:50:37 +0300

    Delete core_homework_3.py

--- core_homework_3.py
+++
@@ -1,8 +0,0 @@
-def get_numbers_ticket(min, max, quantity):
-    import random
-    if min >= 1 and max <=1000 and min < quantity < max:
-        return random.sample(range(min, max), quantity)
-    else:
-        return []
-
-print(get_numbers_ticket(1, 89, 7))
