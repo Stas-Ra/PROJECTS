6826a3e16357ded44be66d60350be421eb483094
Stanislav Rakytianskyi <mail.sovik@gmail.com>
2024-10-06 02:07:41 +0300

    t_3

---
+++ task_3.py
@@ -0,0 +1,23 @@
+import re
+numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+for number in numbers:
+    pattern = r"\D"
+    replacement = ""
+    normalize_phone = re.sub(pattern, replacement, number)
+    print(normalize_phone)
+    def num(normalize_phone):
+
+
+# sanitized_numbers = []
+
