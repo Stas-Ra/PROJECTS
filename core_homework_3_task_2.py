5c7177e1f700345b2fc8fc569e8c9d205fb78a93
Stanislav Rakytianskyi <mail.sovik@gmail.com>
2024-10-09 21:59:00 +0300

    core_homework_3_task_2_edit

--- core_homework_3_task_2.py
+++ core_homework_3_task_2.py
@@ -1,8 +1,8 @@
 def get_numbers_ticket(min, max, quantity):
     import random
-    if min >= 1 and max <=1000 and min < quantity < max:
-        return random.sample(range(min, max), quantity)
+    if min >= 1 and max <=1000 and max - min >= quantity:
+        return random.sample(range(min, max+1), quantity)
     else:
         return []
 
-print(get_numbers_ticket(1, 89, 7))
+print(get_numbers_ticket(1, 5, 4))
